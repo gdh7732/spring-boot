7c34b096e559ac8935e7aefdb13eed4e0bd09778
guodahai <773244804>
2018-05-24 18:17:00 +0800

    更换rocketMq模块

---
+++ rocketmq/src/main/java/com/example/annotation/MQConsumer.java
@@ -0,0 +1,34 @@
+package com.example.annotation;
+
+import com.maihaoche.starter.mq.base.MessageExtConst;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+/**
+ * Created by yipin on 2017/6/27.
+ * RocketMQ消费者自动装配注解
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface MQConsumer {
+    String consumerGroup();
+    String topic();
+
+    /**
+     * 广播模式消费： BROADCASTING
+     * 集群模式消费： CLUSTERING
+     * @return 消息模式
+     */
+    String messageMode() default MessageExtConst.MESSAGE_MODE_CLUSTERING;
+
+    /**
+     * 使用线程池并发消费: CONCURRENTLY("CONCURRENTLY"),
+     * 单线程消费: ORDERLY("ORDERLY");
+     * @return 消费模式
+     */
+    String consumeMode() default MessageExtConst.CONSUME_MODE_CONCURRENTLY;
+    String[] tag() default {"*"};
+}
---
+++ rocketmq/src/test/java/com/example/MQProducerAutoConfigurationTest.java
@@ -0,0 +1,115 @@
+package com.example;
+
+import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
+import com.example.annotation.MQProducer;
+import com.example.base.AbstractMQProducer;
+import com.example.config.MQBaseAutoConfiguration;
+import com.example.config.MQProducerAutoConfiguration;
+import com.maihaoche.starter.mq.annotation.MQProducer;
+import com.maihaoche.starter.mq.base.AbstractMQProducer;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.junit.After;
+import org.junit.Test;
+import org.springframework.boot.test.util.EnvironmentTestUtils;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Component;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+public class MQProducerAutoConfigurationTest {
+
+    private AnnotationConfigApplicationContext context;
+
+    private void prepareApplicationContextEmpty() {
+        this.context = new AnnotationConfigApplicationContext();
+        this.context.register(MQBaseAutoConfiguration.class, MQProducerAutoConfiguration.class);
+        this.context.refresh();
+    }
+
+    private void prepareApplicationContextMissingConfigure() {
+        this.context = new AnnotationConfigApplicationContext();
+        this.context.register(TestProducer.class);
+        MQProducerAutoConfiguration.setProducer(null);
+        this.context.register(MQProducerAutoConfiguration.class);
+        this.context.refresh();
+    }
+
+    private void prepareApplicationContextMissingProducerGroupConfigure() {
+        this.context = new AnnotationConfigApplicationContext();
+        EnvironmentTestUtils.addEnvironment(this.context, "spring.rocketmq.name-server-address:127.0.0.1:9876");
+        this.context.register(TestProducer.class);
+        MQProducerAutoConfiguration.setProducer(null);
+        this.context.register(MQProducerAutoConfiguration.class);
+        this.context.refresh();
+    }
+
+    private void prepareApplicationContextWithoutParent() {
+        this.context = new AnnotationConfigApplicationContext();
+        EnvironmentTestUtils.addEnvironment(this.context, "spring.rocketmq.name-server-address:127.0.0.1:9876");
+        EnvironmentTestUtils.addEnvironment(this.context, "rocketmq.producer-group:test-producer-group");
+        this.context.register(TestProducerNoParent.class);
+        this.context.register(MQProducerAutoConfiguration.class);
+        this.context.refresh();
+    }
+
+    private void prepareApplicationContext() {
+        this.context = new AnnotationConfigApplicationContext();
+        EnvironmentTestUtils.addEnvironment(this.context, "spring.rocketmq.name-server-address:127.0.0.1:9876");
+        EnvironmentTestUtils.addEnvironment(this.context, "spring.rocketmq.producer-group:test-producer-group");
+        this.context.register(TestProducer.class);
+        this.context.register(MQBaseAutoConfiguration.class, MQProducerAutoConfiguration.class);
+        this.context.refresh();
+    }
+
+    @After
+    public void close() {
+        this.context.close();
+    }
+
+
+    @Test
+    public void testEmpty() {
+        prepareApplicationContextEmpty();
+        assertNull(context.getBean(DefaultMQProducer.class));
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testMissingConfigure() {
+        prepareApplicationContextMissingConfigure();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testMissingPGConfigure() {
+        prepareApplicationContextMissingProducerGroupConfigure();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testMissingParent() {
+        prepareApplicationContextWithoutParent();
+    }
+
+    @Test
+    public void testProducerConfiguration() throws Exception {
+        prepareApplicationContext();
+        DefaultMQProducer dp = context.getBean(DefaultMQProducer.class);
+        assertNotNull(dp);
+        assertEquals(dp.getProducerGroup(), "test-producer-group");
+        assertEquals(dp.getNamesrvAddr(), "127.0.0.1:9876");
+    }
+
+
+
+    @Component
+    @MQProducer
+    static class TestProducer extends AbstractMQProducer {
+    }
+
+    @Component
+    @MQProducer
+    static class TestProducerNoParent{
+    }
+
+
+
+}
